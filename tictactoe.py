fee689bcde6cd38094b43b555c9e93cf089400bc
Siddhesh Lakhani <140717222+sid-lakhani@users.noreply.github.com>
2023-09-21 23:04:31 +0530

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -11,6 +11,12 @@ pygame.display.set_caption("Tic Tac Toe")
 
 line_width = 6
 markers = []
+clicked = False
+pos = []
+player = 1
+
+green = (0, 255, 0)
+red = (255, 0, 0)
 
 def draw_grid():
     bg = (255, 255, 200)
@@ -24,17 +30,40 @@ for x in range(3):
     row = [0] * 3
     markers.append(row)
             
-print(markers)
+            
+def draw_markers():
+    x_pos = 0
+    for x in markers:
+        y_pos = 0
+        for y in x:
+            if y == 1:
+              pygame.draw.line(screen, green, (x_pos * 200 + 15, y_pos * 200 + 15), (x_pos * 200 + 185, y_pos * 200 + 185), line_width*2)
+              pygame.draw.line(screen, green, (x_pos * 200 + 15, y_pos * 200 + 185), (x_pos * 200 + 185, y_pos * 200 + 15), line_width*2)
+            if y == -1:
+                pygame.draw.circle(screen, red, (x_pos * 200 + 100, y_pos * 200 + 100), 85, line_width*2)
+            y_pos += 1
+        x_pos += 1
 
 running = True
 while running:
     
     draw_grid()
+    draw_markers()
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        if event.type == MOUSEBUTTONDOWN and clicked == False:
+            clicked = True
+        if event.type == pygame.MOUSEBUTTONUP and clicked == True:
+            clicked == False
+            pos = pygame.mouse.get_pos()
+            cell_x = pos[0]
+            cell_y = pos[1]
+            if markers [cell_x // 200][cell_y // 200] == 0:
+                markers [cell_x // 200][cell_y // 200] = player
+                player *= -1
+            
     pygame.display.update()
 
 pygame.quit()
